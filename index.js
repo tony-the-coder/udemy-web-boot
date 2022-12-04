a5e78bfd309faa4f9f0793d5a3741229e5ea067d
Tony <tonythecoder@outlook.com>
2022-12-04 17:12:48 -0500

    I followed my own way for this Udemy project

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
-// document.querySelector("p").style.backgroundColor = "red";
-// document.querySelector("h1").innerHTML = "Test";
-// document.querySelector("p").style.backgroundColor = "pink";
-// document.firstElementChild.lastElementChild.lastElementChild.lastElementChild.innerHTML = "Test";
-// document.firstElementChild.lastElementChild.lastElementChild.previousElementSibling.lastElementChild.innerHTML = "tesst";
+//Random number for the dice game my version
 
-// document.getElementsByTagName("li")[2].innerHTML = "Ulrich"
-document.getElementsByClassName("item")
+function diceGame() {
+
+    let ranNum1 = Math.floor((Math.random() * 6) + 1);
+    let ranNum2 = Math.floor((Math.random() * 6) + 1);
+
+    // Update the image my version
+
+    document.querySelector(".img1").setAttribute("src", "/images/dice" + ranNum1 + ".png")
+    document.querySelector(".img2").setAttribute("src", "/images/dice" + ranNum2 + ".png")
 
 
+
+    ///Udemy Version
+    /// The Udemy instructions have eight lines of code compared to my four, I did not feel like continuing to type because it was more work than what I had. Programming should be more concise. I am also learning Python, maybe that is why I am thinking "Pythonic" or something like that. 
+
+    /// Beginning easy part of a Dicee Game
+    if (ranNum1 > ranNum2) {
+        console.log("First die Wins with a " + ranNum1 + " vs a " + ranNum2)
+        document.querySelector("h1").textContent = "Player One Wins"
+    }
+    else if (ranNum2 === ranNum1) {
+        document.querySelector("h1").textContent = "It is a Tie"
+        console.log("tie " + ranNum1 + " and " + ranNum2)
+    }
+    else {
+
+        document.querySelector("h1").textContent = "Player Two Wins"
+        console.log("Second Die Wins with a " + ranNum2 + " vs " + ranNum1)
+    }
+}
