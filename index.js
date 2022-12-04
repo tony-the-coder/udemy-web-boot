99c655d6a4aa23c4d7cd33b04f0c1c129c40ee8b
Anthony Aldea <tonythecoder@outlook.com>
2022-12-04 12:53:26 -0500

    From the CLI because VSCode.dev is being a real pain.

---
+++ index.js
@@ -0,0 +1,10 @@
+// document.querySelector("p").style.backgroundColor = "red";
+// document.querySelector("h1").innerHTML = "Test";
+// document.querySelector("p").style.backgroundColor = "pink";
+// document.firstElementChild.lastElementChild.lastElementChild.lastElementChild.innerHTML = "Test";
+// document.firstElementChild.lastElementChild.lastElementChild.previousElementSibling.lastElementChild.innerHTML = "tesst";
+
+// document.getElementsByTagName("li")[2].innerHTML = "Ulrich"
+document.getElementsByClassName("item")
+
+
